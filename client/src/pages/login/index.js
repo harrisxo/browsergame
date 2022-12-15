f8bd090e7830d5ccfd53ae85048176d241c61c89
Heitor Sardinha <heitorpaiva2012@gmail.com>
2022-12-15 18:49:52 -0300

    Client: set up login route

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,11 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function Login() {
+const Login = () => {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [authenticatedUser, setAuthenticatedUser] = useState({});
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser((previousValue) => {
+      return {
+        ...previousValue,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await axios
+      .post("/api/v1/login", user)
+      .then((res) => {
+        setIsAuthenticated(true);
+        setAuthenticatedUser(res.data);
+        console.log("User authenticated!");
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <h1>This is the login page.</h1>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          <li>
+            <label for="username"> Username: </label>
+            <input
+              type="text"
+              id="username"
+              value={user.username}
+              name="username"
+              onChange={(event) => {
+                onInputChange(event);
+              }}
+            />
+          </li>
+          <li>
+            <label for="password"> Password: </label>
+            <input
+              type="password"
+              id="password"
+              value={user.password}
+              name="password"
+              onChange={(event) => {
+                onInputChange(event);
+              }}
+            />
+          </li>
+          <input type="submit" value="Submit" />
+        </ul>
+      </form>
     </div>
   );
-}
+};
 
 export default Login;
