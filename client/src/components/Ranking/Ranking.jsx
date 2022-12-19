0748baea0d76faaca3cf9decea316fb0a933804c
Aryse Tansy <medictansy@gmail.com>
2022-12-19 15:47:17 -0500

    Rank Component Client

---
+++ client/src/components/Ranking/Ranking.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import classes from "./Ranking.module.css";
+const Ranking = ({ setShowRanking }) => {
+  const [rank, setRank] = useState([]);
+
+  const fetchRank = async () => {
+    await axios
+      .get("/api/v1/rank")
+      .then((serverRes) => setRank(serverRes.data.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchRank();
+  }, []);
+
+  return (
+    <article className={classes.article}>
+      <h2 className={classes.h2}>
+        RANK{" "}
+        <span className={classes.close} onClick={() => setShowRanking(false)}>
+          x
+        </span>
+      </h2>
+      {rank.map((player) => (
+        <p className={classes.p}>
+          - {player.username}: Lvl-{player.level}
+        </p>
+      ))}
+    </article>
+  );
+};
+
+export default Ranking;
